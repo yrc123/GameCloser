c2400b5abb1c1e665c97b84b959f445f8d263f1b
yrc <958480638@qq.com>
2021-10-04 23:43:37 +0800

    添加跨域请求

--- GameCloserService/src/main/java/com/yrc/gamecloserservice/controller/GameCloserController.java
+++ GameCloserService/src/main/java/com/yrc/gamecloserservice/controller/GameCloserController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import com.yrc.gamecloserservice.util.ResponseUtil;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(originPatterns = "*", allowedHeaders = "*")
 public class GameCloserController {
     GameCloserSocketService gameCloserSocketService;
 
