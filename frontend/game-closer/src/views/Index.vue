084cffabb0ee3d854350d968a49477444637586f
Savona <905899183@qq.com>
2021-10-05 00:03:44 +0800

    [Updated] 好了好累

--- frontend/game-closer/src/views/Index.vue
+++ frontend/game-closer/src/views/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="content">
-		<div class="" style="display: flex;">
+		<div class="a" style="display: flex;">
 			<div class="select">
 				<el-select v-model="value" placeholder="请选择主机">
 					<el-option
@@ -14,11 +14,22 @@
 			<div class="process">
 				<el-input v-model="processName" placeholder="请输入进程名"></el-input>
 			</div>
+			<div class="usually-process">
+				<el-select v-model="processName" placeholder="请选择常用进程">
+					<el-option
+						v-for="item in usuallyProcess"
+						:key="item.value"
+						:label="item.hostname"
+						:value="item.value">
+					</el-option>
+				</el-select>
+			</div>
 		</div>
 		<div class="send">
 			
 		</div>
-		<el-button type="primary">主要按钮</el-button>
+		<el-button type="primary" @click="send">发送</el-button>
+		<el-button type="primary" @click="getResult" style="margin-top: 20px;">获取结果</el-button>
   </div>
 </template>
 
@@ -33,6 +44,7 @@ export default {
 				hostNameList:[],
 				value:'',
 				processName:'',
+				usuallyProcess:[],
 			}
 		},
   components: {
@@ -44,16 +56,22 @@ export default {
 	methods:{
 		init(){
 			var _this=this;
-			// axios.get('http://127.0.0.1:8083/devices')
-			// .then(function(response){
-			// 	console.log(response)
-			// 	let arr=response.data.deviceList;
-			// 	for (var i = 0; i < arr.length; i++) {
-			// 		arr[i].value=i;
-			// 	}
-			// 	_this.hostNameList=arr;
-			// });
+			axios.get('http://127.0.0.1:8083/api/devices')
+			.then(function(response){
+				console.log(response.data.data.deviceList)
+				let arr=response.data.deviceList;
+				for (var i = 0; i < arr.length; i++) {
+					arr[i].value=i;
+				}
+				_this.hostNameList=arr;
+			});
 		},
+		send(){
+			console.log('发送请求');
+		},
+		getResult(){
+			console.log('获取结果'); 
+		}
 	},
 }
 </script>
@@ -71,9 +89,12 @@ export default {
 	}
 	
 	.select{
-		margin-right: 20px;
 	}
 	.send{
-
+		margin-top: 20px;
+	}
+	.process{
+		margin-left: 20px;
+		margin-right: 20px;
 	}
 </style>
